8975aae27e6cde5f19fcdd7213bfab8a224c1533
Hsing-I Wang <hsing.sophie@gmail.com>
2021-01-06 01:00:33 +0000

    practice object

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  for (let k of Object.keys(obj)) {
+    if (obj[k] === value) {
+      return k;
+    }
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  test: "Test Value"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Test Value"), "test");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Test Value Two"), undefined);
