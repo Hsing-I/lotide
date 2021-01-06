e42fdf7ef3aab4b56a659eb25fc87123d1430d41
Hsing-I Wang <hsing.sophie@gmail.com>
2021-01-06 17:54:46 +0000

    check two objects to see if they are equal

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,13 +1,49 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } 
+    return true;
+  }
+};
+
 const assertObjectsEqual = function(actual, expected) {
   // Implement me!
   const inspect = require('util').inspect;
-  console.log(`Example label: ${inspect(actual)}`);
+  let arr1 = Object.keys(actual);
+  let arr2 = Object.keys(expected);
+
+  if (arr1.length !== arr2.length) {
+    return `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;   
+  }
+
+  if (arr1.length == 0 || arr2.length == 0){
+    return `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`; 
+  }
+  
+  for(let key1 of arr1){
+    if(typeof actual[key1] !==  typeof expected[key1]){
+      return `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`; 
+    }else if(Array.isArray(actual[key1]) && Array.isArray(expected[key1])){
+      eqArrays(actual[key1], expected[key1]);
+    }else{
+      if(actual[key1] !== expected[key1]){
+        return `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`; 
+      }
+    }  
+  }
+  return `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`; 
 };
 
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 console.log(assertObjectsEqual(ab, ba)); // => true
+
 
 const abc = { a: "1", b: "2", c: "3" };
 console.log(assertObjectsEqual(ab, abc)); // => false
@@ -16,5 +52,6 @@ const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 console.log(assertObjectsEqual(cd, dc)); // => true
 
+const cd1 = {};
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(assertObjectsEqual(cd, cd2)); // => false
+console.log(assertObjectsEqual(cd1, cd2)); // => false
