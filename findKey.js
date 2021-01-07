0f9f75ccc3fbd5dd5a9d0c92067ec9f87f21849f
Hsing-I Wang <hsing.sophie@gmail.com>
2021-01-07 17:34:44 +0000

    find the key of certain value

---
+++ findKey.js
@@ -0,0 +1,18 @@
+const findKey = function(obj, callback) {
+  for(const key of Object.keys(obj)){
+    if(callback(obj[key])){
+      return key;
+    }
+  }  
+}
+
+const find = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3); 
+
+console.log(find);
