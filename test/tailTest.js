ee93e7c184af377a738346f68ca73af95668c89d
Hsing-I Wang <hsing.sophie@gmail.com>
2021-01-10 01:03:13 +0000

    writing unit test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,16 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../head');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const arr = ['Lighthouse', 'Labs'];
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), arr);
+  });
+});
+
+/*const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3);
 assertEqual(words[0], "Yo Yo");
 assertEqual(words[1], "Test");
-assertEqual(words[2], "Labs");
+assertEqual(words[2], "Labs");*/
